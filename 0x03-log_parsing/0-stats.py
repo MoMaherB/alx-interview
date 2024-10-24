127e1ffe38330d3dbc5ebba132af3bedf06ed472
MoMaherB <maher1525@gmail.com>
2024-10-25 02:29:25 +0300

    parcelog

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,6 +4,37 @@ A script: Reads standard input line by line and computes metrics
 """
 
 
+def parseLogs():
+    """
+    script that reads stdin line by line and computes metrics
+    """
+    stdin = __import__('sys').stdin
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for args in stdin:
+            lineNumber += 1
+            args = args.split()
+            try:
+                fileSize += int(args[-1])
+                if args[-2] in codes:
+                    try:
+                        statusCodes[args[-2]] += 1
+                    except KeyError:
+                        statusCodes[args[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if lineNumber == 10:
+                status_code(fileSize, statusCodes)
+                lineNumber = 0
+        status_code(fileSize, statusCodes)
+    except KeyboardInterrupt as e:
+        status_code(fileSize, statusCodes)
+        raise
+
+
 def status_code(fileSize, statusCodes):
     """
     Prints generated status codt to standard output
@@ -13,28 +44,5 @@ def status_code(fileSize, statusCodes):
         print("{}: {}".format(key, value))
 
 
-stdin = __import__('sys').stdin
-lineNumber = 0
-fileSize = 0
-statusCodes = {}
-codes = ('200', '301', '400', '401', '403', '404', '405', '500')
-try:
-    for args in stdin:
-        lineNumber += 1
-        args = args.split()
-        try:
-            fileSize += int(args[-1])
-            if args[-2] in codes:
-                try:
-                    statusCodes[args[-2]] += 1
-                except KeyError:
-                    statusCodes[args[-2]] = 1
-        except (IndexError, ValueError):
-            pass
-        if lineNumber == 10:
-            status_code(fileSize, statusCodes)
-            lineNumber = 0
-    status_code(fileSize, statusCodes)
-except KeyboardInterrupt as e:
-    status_code(fileSize, statusCodes)
-    raise
+if __name__ == '__main__':
+    parseLogs()
