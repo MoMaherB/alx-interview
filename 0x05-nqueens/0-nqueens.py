32c2af150666b1748e3c769d7c1a69d4551c0b13
MoMaherB <maher1525@gmail.com>
2024-11-07 23:31:55 +0200

    N

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import sys
+
+
+def print_solutions(solutions):
+    """Print solutions in the required format."""
+    for solution in solutions:
+        print(solution)
+
+
+def is_safe(board, row, col):
+    """Check if placing a queen at (row, col) is safe."""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens(N, row, board, solutions):
+    """Recursive function to solve N-Queens problem."""
+    if row == N:
+        solutions.append([[i, board[i]] for i in range(N)])
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(N, row + 1, board, solutions)
+            board[row] = -1
+
+
+def main():
+    """Check for the correct number of arguments"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solutions = []
+
+    solve_nqueens(N, 0, board, solutions)
+
+    print_solutions(solutions)
+
+
+if __name__ == "__main__":
+    main()
